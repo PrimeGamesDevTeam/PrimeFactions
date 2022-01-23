86c29fa74c63a952e8ec20153b38c4bc6fcbd7bb
Nasir Idrishi <nasiridrishi@outlook.com>
2022-01-24 00:59:00 +0530

    init setups

---
+++ src/main/java/net/primegames/primefactions/PrimeFactions.java
@@ -0,0 +1,17 @@
+package net.primegames.primefactions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrimeFactions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
